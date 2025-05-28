8dc4726af00fefeb1101b67a07512e85b74ce827
IT21314742 <it21314742@my.sliit.lk>
2025-05-28 11:07:40 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ job_role = st.text_input("Enter the job role you're targeting (optional)")
 
 analyze = st.button("Analyze resume")
 
-
+def extract_text_from_pdf(pdf_)
 
 def extract_text_from_file(uploded_file):
     if uploaded_file.type == "application/pdf":
